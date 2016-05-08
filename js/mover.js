cc55a6a014ca72ba3283fe7108746065d7a07f11
pgarnham <pgarnham@uc.cl>
2016-05-08 13:30:17 -0300

    despliegue de paises según elección
    al apretar un pais en page1.html desaparece américa y aparece chile y el
    país elegido

--- js/mover.js
+++ js/mover.js
@@ -1,7 +1,201 @@
-$("#chile")
-    .delay(500)
-      .velocity({ x: "+=200", y: "25%" })
-    .velocity({ fillRed: 255, strokeWidth: 2 })
-      .velocity({ height: 50, width: 50 })
-    .velocity({ rotateZ: 90, scaleX: 0.5 })
-    .velocity("reverse", { delay: 250 });
+
+
+
+$(document).ready(function(){
+
+		$("#canada").click(function(){
+
+			$("#can").show("slow");
+			$(".st0").hide("slow");
+			$("#chil").show("slow");
+
+		});
+
+		$("#usa").click(function(){
+
+			$("#us").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#bolivia").click(function(){
+
+			$("#bol").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#peru").click(function(){
+
+			$("#per").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#argentina").click(function(){
+
+			$("#ar").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#brazil").click(function(){
+
+			$("#bra").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#uruguay").click(function(){
+
+			$("#ur").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#paraguay").click(function(){
+
+			$("#par").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#ecuador").click(function(){
+
+			$("#ec").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#colombia").click(function(){
+
+			$("#col").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#venezuela").click(function(){
+
+			$("#ven").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#guyana").click(function(){
+
+			$("#guy").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#surinam").click(function(){
+
+			$("#sur").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#guayana_francesa").click(function(){
+
+			$("#gua").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#panama").click(function(){
+
+			$("#pan").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#costa_rica").click(function(){
+
+			$("#cos").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#nicaragua").click(function(){
+
+			$("#nic").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#honduras").click(function(){
+
+			$("#hon").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#salvador").click(function(){
+
+			$("#sal").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#belice").click(function(){
+
+			$("#bel").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#guatemala").click(function(){
+
+			$("#gua").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#cuba").click(function(){
+
+			$("#cub").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#haiti").click(function(){
+
+			$("#hai").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#mexico").click(function(){
+
+			$("#mex").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#republica_dominicana").click(function(){
+
+			$("#rep").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		$("#bahamas").click(function(){
+
+			$("#bah").show("slow");
+			$("#america").hide("slow");
+			$("#chil").show("slow");
+		});
+
+		
+
+
+
+
+});
+
+
+		
+		
+
+
+	

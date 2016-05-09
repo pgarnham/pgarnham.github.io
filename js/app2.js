26f0d361895e1e1baf32910da5c974d0860f672f
pgarnham <pgarnham@uc.cl>
2016-05-09 09:04:16 -0300

    mejora elegir otro país
    al presionar el botón no recarga la pagina (usa javascript)...

--- js/app2.js
+++ js/app2.js
@@ -1,3 +1,15 @@
+
+
+
+
+
+
+
+
+
+
+
+
 $(function () {
     $('#container').highcharts({
         chart: {
--- js/app3.js
+++ js/app3.js
@@ -1,3 +1,7 @@
+
+
+
+
 $(function () {
     $('#container2').highcharts({
         chart: {
